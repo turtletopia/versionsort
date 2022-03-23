f65d035a5133669945115f1f5acda27f5b97d1d2
Laura <erdaradun.gaztea@gmail.com>
2022-03-23 10:04:15 +0100

    add more complicated test cases to ver_latest

--- tests/testthat/test-latest.R
+++ tests/testthat/test-latest.R
@@ -23,3 +23,18 @@ test_that("ver_latest works", {
     xtable_versions[length(xtable_versions)]
   )
 })
+
+test_that("ver_latest considers no component to be alphanumerically first", {
+  expect_equal(
+    ver_latest(sample(c("0.2.2", "0.2.5", "0.2.5.2", "0.2.5.3", "0.2.5.4"))),
+    "0.2.5.4"
+  )
+})
+
+test_that("ver_latest extracts number subcomponent correctly", {
+  # In other words, 10 > 9 (including 9b8a) in the case presented below
+  expect_equal(
+    ver_latest(sample(c("3.10-0 (1999/06/26)", "3.9b8a-2 (1999/06/07)"))),
+    "3.10-0 (1999/06/26)"
+  )
+})
